15fa31373d1fb53052451b51e4c59ecc9ce4a11d
PeterR <solutionsgelo@gmail.com>
2019-06-11 15:27:22 +0200

    created the basic Frontend structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+
 import './App.css';
+import AuthPage from './components/AuthPage';
+import EventsPage from './components/EventsPage';
+import BookingsPage from './components/BookingsPage';
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>Im the app btw</h1>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Redirect from="/" exact to="/auth" />
+        <Route path="/auth" exact component={AuthPage} />
+        <Route path="/events" exact component={EventsPage} />
+        <Route path="/bookings" exact component={BookingsPage} />
+      </Switch>
+    </BrowserRouter>
   );
 };
 

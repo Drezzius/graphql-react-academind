491e85cf5b6480c04b8dc956c511b9f4cb8ba986
PeterR <solutionsgelo@gmail.com>
2019-06-11 16:17:28 +0200

    Added/styled navbar

--- client/src/App.js
+++ client/src/App.js
@@ -5,16 +5,20 @@ import './App.css';
 import AuthPage from './pages/AuthPage';
 import EventsPage from './pages/EventsPage';
 import BookingsPage from './pages/BookingsPage';
+import MainNavigation from './components/Navigation/MainNavigation';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Switch>
-        <Redirect from="/" exact to="/auth" />
-        <Route path="/auth" exact component={AuthPage} />
-        <Route path="/events" exact component={EventsPage} />
-        <Route path="/bookings" exact component={BookingsPage} />
-      </Switch>
+      <MainNavigation />
+      <main className="main-content">
+        <Switch>
+          <Redirect from="/" exact to="/auth" />
+          <Route path="/auth" exact component={AuthPage} />
+          <Route path="/events" exact component={EventsPage} />
+          <Route path="/bookings" exact component={BookingsPage} />
+        </Switch>
+      </main>
     </BrowserRouter>
   );
 };
